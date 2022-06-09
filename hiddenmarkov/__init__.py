dbd6ca86a3077d46bb24ef0186cc2e7b036ff8c2
sildater <41552783+sildater@users.noreply.github.com>
2022-06-09 17:31:51 +0200

    add windowed HMM

--- hiddenmarkov/__init__.py
+++ hiddenmarkov/__init__.py
@@ -5,7 +5,7 @@ Hidden Markov Models
 This module contains case classes to define Hidden Markov Models.
 """
 import pkg_resources
-
+import warnings
 import numpy as np
 from collections import defaultdict
 
@@ -63,7 +63,8 @@ class HiddenMarkovModel(object):
     """
 
     def __init__(self, observation_model,
-                 transition_model, state_space=None):
+                 transition_model, 
+                 state_space=None):
 
         self.observation_model = observation_model
         self.transition_model = transition_model
@@ -73,13 +74,24 @@ class HiddenMarkovModel(object):
         else:
             self.state_space = np.arange(self.n_states)
 
-    def find_best_sequence(self, observations, log_probabilities=True):
-        best_sequence, sequence_likelihood = viterbi_algorithm_optimized(
+    def find_best_sequence(self, 
+                           observations, 
+                           log_probabilities=True,
+                           viterbi="optimized"):
+        if viterbi == "optimized":
+            viterbi_fun = viterbi_algorithm_optimized
+        elif viterbi == "windowed":
+            viterbi_fun = viterbi_algorithm_optimized_windowed
+        elif viterbi == "legacy":
+            viterbi_fun = viterbi_algorithm
+        else:
+            warnings.warn("viterbi needs to be 'optimized', 'windowed', or 'legacy'")
+            return
+        best_sequence, sequence_likelihood = viterbi_fun(
             hmm=self,
             observations=observations,
             log_probabilities=log_probabilities)
         return best_sequence, sequence_likelihood
-
 
 # alias
 HMM = HiddenMarkovModel
@@ -252,6 +264,120 @@ def viterbi_algorithm_optimized(hmm, observations, log_probabilities=True):
     return best_sequence, path_likelihood
 
 
+def viterbi_algorithm_optimized_windowed(hmm, 
+                                         observations, 
+                                         log_probabilities=True):
+    """
+    Find the most probable sequence of latent variables given
+    a sequence of observations.
+    
+    !!! This version uses a transition model with fixed window of
+    states (see WindowedHiddenMarkiovModel). This window represents
+    the currently "visible" states from any of an underlying
+    longer sequence of global states.
+    
+    The viterbi uses the current best path at each step to 
+    update the global path. This effectively limits the path
+    to corridor in the states. The observation model is called 
+    with a global state id, so the true, underlying states
+    can have different probabilities.
+      
+
+    Parameters
+    ----------
+    observations: iterable
+       An iterable containing observations. The type of each
+       element depends on input types accepted by the
+       `hmm.observation_model`
+    log_probabilities: Bool (optional)
+       If True, uses log probabilities to compute the Viterbi
+       recursion (better for numerical stability). Default is True.
+
+    Returns
+    -------
+    path: np.ndarray
+        The most probable sequence of latent variables
+    likelihood: float
+        The likelihood (either the probability or the
+        log proability if `log_probabilities` is True)
+        of the best sequence.
+    """
+    # Set whether to use log probabilities in transition and
+    # observation models
+    hmm.transition_model.use_log_probabilities = log_probabilities
+    hmm.observation_model.use_log_probabilities = log_probabilities
+    # Initialize matrix for holding the best sub-sequence
+    # (log-)likelihood
+    omega = np.zeros((len(observations), hmm.n_states))
+    # Initialize matrix for holding the best sub-sequence idx
+    omega_idx = np.zeros((len(observations), hmm.n_states), dtype=int)
+    
+    # Initialize vector for holding the current best idx
+    # Current window id; start at state 0 and keep the state at the start of the current window
+    current_window_idx = np.zeros((len(observations)+1), dtype=int)
+
+    omega_idx[0, :] = 0
+    current_window_idx[0] = 0 # zero is a dummy state just for jump size
+    current_window_idx[1] = 0 # zero is the actual first window idx
+
+    # Initiate for i == 0
+    obs_prob, _ = hmm.observation_model(observations[0], current_window_idx[0])
+
+    if log_probabilities:
+        omega[0, :] = obs_prob + hmm.transition_model.init_distribution
+    else:
+        omega[0, :] = obs_prob * hmm.transition_model.init_distribution
+
+    # Viterbi recursion
+    if log_probabilities:
+        for i, obs in enumerate(observations[1:], 1):
+            obs_prob, glob_ref_idx = hmm.observation_model(obs, current_window_idx[i])
+            # omega slice is a row vector, transition_model is a matrix 
+            # of prob from state id_row to state id_column
+            # use only the slice of omega that is shifted by the previous jump 
+            previous_jump = current_window_idx[i] - current_window_idx[i-1]
+            prob_of_jump_to_state = np.concatenate((
+                                                    omega[i - 1, previous_jump:],
+                                                    np.ones(previous_jump)*-np.inf
+                                                    )) + \
+                                    hmm.transition_model().T
+            state = np.argmax(prob_of_jump_to_state, axis = 1)
+            prob = prob_of_jump_to_state[np.arange(hmm.n_states),state]
+            omega[i, :] = obs_prob + prob
+            omega_idx[i, :] = state
+            # slide the window to the current best
+            current_best_sequence_idx = omega[i, :].argmax()
+            current_window_idx[i+1] = glob_ref_idx[current_best_sequence_idx]   
+            
+    else:
+        for i, obs in enumerate(observations[1:], 1):
+            obs_prob, glob_ref_idx = hmm.observation_model(obs, current_window_idx[i])
+            # omega slice is a row vector, transition_model is a matrix 
+            # of prob from state id_row to state id_column
+            # use only the slice of omega that is shifted by the previous jump
+            previous_jump = current_window_idx[i] - current_window_idx[i-1]
+            prob_of_jump_to_state = np.concatenate((
+                                                    omega[i - 1, previous_jump:],
+                                                    np.zeros(previous_jump)
+                                                    )) * \
+                                    hmm.transition_model().T
+            state = np.argmax(prob_of_jump_to_state, axis = 1)
+            prob = prob_of_jump_to_state[np.arange(hmm.n_states),state]
+            omega[i, :] = obs_prob * prob
+            omega_idx[i, :] = state
+            # slide the window to the current best
+            current_best_sequence_idx = omega[i, :].argmax()
+            current_window_idx[i+1] = glob_ref_idx[current_best_sequence_idx]
+
+    # Get best path (backtracking!)
+    # Get index of the best state
+    best_sequence_idx = omega[-1, :].argmax()
+    # likelihood of the path
+    path_likelihood = omega[-1, best_sequence_idx]
+
+    return current_window_idx[1:], path_likelihood
+
+
 class ConstantTransitionModel(object):
     """
     Constant Transition Model
@@ -380,3 +506,111 @@ class CategoricalStringObservationModel(ObservationModel):
     def __call__(self, observation, *args, **kwargs):
         idx = self.observations.index(observation)
         return self.observation_probabilities[idx]
+
+
+class WindowedObservationModel(ObservationModel):
+    """
+    Windowed Observation Model
+    Uses a list of state-specific probability models
+    as observation model. Probability models can also be distances,
+    in which case the different distances are inverted and softmaxed.
+    
+    Only works with viterbi_algorithm_optimized_windowed
+    
+    Parameters
+    ----------
+    prob_models_at_state : list of tuples
+        A list of tuples (probability_model,prob_model_state_id) 
+        indexed by state_id
+    
+    Attributes
+    ----------
+    prob_models_at_state: list of tuples
+
+    """
+    def __init__(
+            self,
+            prob_models_at_state,
+            use_inverted_probs=True,
+            use_log_probabilities=True
+    ):
+        super().__init__(use_log_probabilities=use_log_probabilities)
+        self.use_log_probabilities = use_log_probabilities
+        self.use_inverted_probs = use_inverted_probs
+        self.prob_models_at_state = prob_models_at_state
+        self.state_number = len(prob_models_at_state)
+        if self.use_inverted_probs:
+            if self.use_log_probabilities:
+                self.outfunc = lambda d : np.log(inverted_softmax(np.array(d)))
+            else:
+                self.outfunc = lambda d : inverted_softmax(np.array(d))
+        else:
+            if self.use_log_probabilities:
+                self.outfunc = lambda d : np.log(softmax(np.array(d)))
+            else:
+                self.outfunc = lambda d : softmax(np.array(d))
+
+        
+    def __call__(self, observation, current_state, *args, **kwargs):
+        # give the current state pick the right states 
+        prob_models = self.prob_models_at_state[max(0,min(current_state,self.state_number-1))]
+        # compute the probability for all models
+        dists = list()
+        glob_ref_idx = list()
+        
+        for prob_model, idx in prob_models: 
+            dists.append(prob_model(observation))
+            glob_ref_idx.append(idx)
+        
+        return self.outfunc(dists), np.array(glob_ref_idx)
+ 
+    
+def inverted_softmax(array):
+    """
+    array of distances (non-negative) converted to probabilities
+    the lowest distance has the highest probability
+    """
+    return np.exp(-array)/np.sum(np.exp(-array))
+
+
+def softmax(array):
+    """
+    array of estimations converted to probabilities
+    the highest prob has the highest probability
+    """
+    return np.exp(array)/np.sum(np.exp(array))
+
+
+def create_prob_models(no_global_states = 100, 
+                       window_size = 5):
+    """
+    example function to create a
+    list of state-specific probability models
+    for use with viterbi_algorithm_optimized_windowed
+    and WindowedObservationModel
+    
+    Parameters
+    ----------
+    no_global_states : int
+        number of global states in the HMM
+    window_size : int
+        number of states visible to the transition model
+           
+    Attributes
+    ----------
+    prob_models_at_state: dict
+
+    """
+    models_per_state = list()
+    for i in range(no_global_states):
+        # for each global state, create list of prob models 
+        # corresponding to local states in the window
+        models = list()
+        for j in range(window_size):
+            # for each local state, create a prob models
+            def probf(input, ref = i+j):
+                # dummy prob function
+                return int(input == ref)
+            models.append((probf, max(0,min(no_global_states-1,i+j))))
+        models_per_state.append(models)
+    return models_per_state
